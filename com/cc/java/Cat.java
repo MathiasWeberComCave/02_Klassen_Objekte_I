9b8bd953a8ba4db008efcc67eac63303c5d84c06
Peter <schmidt.multimedia@gmail.com>
2022-11-23 10:07:27 +0100

    Felder / Methoden

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,15 +1,26 @@
 package com.cc.java;
 
 public class Cat {
-    
-public void tellYourAddress(){
-   // Aufruf der Methode output() in der KLASSE App
-   // this ist die Referenz des Objektes auf sich selbst
-    App.output("Blick von innen: " + this);
-}
+ 
+   public String name;
+   public String furColor;
+   public int age;
 
+    public String tellYourName() {
+       return this.name;
+    }
 
+    public String tellYourFurColor() {
+        return this.furColor;
+    }
 
+    public int tellYourAge() {
+        return this.age;
+    }
+
+    public void tellYourAddress(){
+        App.output("Blick von innen: " + this);
+    }
 
 
     
